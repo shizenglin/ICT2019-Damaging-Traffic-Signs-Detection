4c46a67aaed5c294abf2d1fe6999dc584d91d19a
Ivan Sosnovik <sosnovikivan@gmail.com>
2019-01-22 23:42:58 +0100

    add GTSRB_Seq

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -1,10 +1,13 @@
 import os
 import ntpath
 import copy
+import glob
 
-from torchvision.datasets import ImageFolder
-
+from PIL import Image
 import pandas as pd
+
+import torch
+from torchvision.datasets import ImageFolder
 
 
 def _get_all_csv_paths(dir):
@@ -46,3 +49,47 @@ class GTSRB_Damaged(ImageFolder):
         image_path = self.samples[index][0]
         return image, label, self.damage_dict[image_path]
 
+
+class GTSRB_Seq(torch.utils.data.Dataset):
+    
+    num_classes = 43
+    images_per_sign = 30
+
+    def __init__(self, root, transform=None):
+        super().__init__()
+        root = os.path.expanduser(root)
+        paths = _get_all_csv_paths(root)
+        self.damage_dict = _join_csv_into_dict_by_paths(paths)
+        self.transform = transform
+        self.sequences = []
+        
+        for class_idx in range(GTSRB_Seq.num_classes):
+            dir_name = os.path.join(root, '{:05d}/*.ppm'.format(class_idx))
+            num_files = len(glob.glob(dir_name))
+            num_seqs = num_files // GTSRB_Seq.images_per_sign
+            
+            for seq_idx in range(num_seqs):
+                seq = []
+                for frame_idx in range(GTSRB_Seq.images_per_sign):
+                    image_path = os.path.join(root, '{:05d}/{:05d}_{:05d}.ppm'.format(
+                        class_idx, seq_idx, frame_idx
+                    ))
+                    
+                    if os.path.exists(image_path):
+                        seq.append((image_path, class_idx, self.damage_dict[image_path]))
+                        
+                self.sequences.append(seq)
+
+    def __getitem__(self, index):
+        """
+        Return:
+            image_tensors, class_labels, damage_labels
+        """
+        seq = self.sequences[index]
+        images = [Image.open(s[0]) for s in seq]
+        if self.transform:
+            images = [self.tranform(img) for img in images] 
+        return images, [s[1] for s in seq], [s[2] for s in seq]
+        
+    def __len__(self):
+        return len(self.sequences)
