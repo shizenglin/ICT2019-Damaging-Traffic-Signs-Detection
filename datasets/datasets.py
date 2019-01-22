2b921f44e2efbc54beb7e5504dbbc6452e7377cd
Ivan Sosnovik <sosnovikivan@gmail.com>
2019-01-22 23:41:15 +0100

    Update datasets.py

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -5,8 +5,6 @@ import copy
 from torchvision.datasets import ImageFolder
 
 import pandas as pd
-import numpy as np
-from sklearn.model_selection import train_test_split
 
 
 def _get_all_csv_paths(dir):
@@ -35,7 +33,7 @@ def _join_csv_into_dict_by_paths(paths):
 class GTSRB_Damaged(ImageFolder):
 
     def __init__(self, root, transform=None, target_transform=None):
-        super().__init__(root=root, transform=transform, target_transform=target_transform)
+        super(GTSRB_Damaged, self).__init__(root=root, transform=transform, target_transform=target_transform)
         paths = _get_all_csv_paths(root)
         self.damage_dict = _join_csv_into_dict_by_paths(paths)
 
@@ -44,25 +42,7 @@ class GTSRB_Damaged(ImageFolder):
         Return:
             image_tensor, class_label, damage_label
         """
-        image, label = super().__getitem__(index)
+        image, label = super(GTSRB_Damaged, self).__getitem__(index)
         image_path = self.samples[index][0]
         return image, label, self.damage_dict[image_path]
 
-
-def split_ImageFolder(dataset, test_size=0.2, seed=42):
-    """
-    Return:
-        train_dataset, test_dataset
-    """
-    np.random.seed(seed)
-    samples = dataset.samples
-    train, test = train_test_split(samples, test_size=test_size)
-    train_dataset = copy.copy(dataset)
-    test_dataset = copy.copy(dataset)
-
-    train_dataset.samples = train
-    train_dataset.imgs = train
-
-    test_dataset.samples = test
-    test_dataset.imgs = test
-    return train_dataset, test_dataset
