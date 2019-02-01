fa194d05776cfdc4b420d937c16bb832617e4d69
Jeroen Delcour <jeroen.delcour@orangenxt.com>
2019-02-01 17:10:56 +0100

    working live webcam demo

--- codes/final_clean/demo.py
+++ codes/final_clean/demo.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy as np
-from tqdm import tqdm
+import cv2
 
 import torch
 import torch.nn as nn
@@ -12,6 +12,10 @@ from torchvision import datasets, transforms
 
 from Cutout.model.resnet import ResNet18
 
+vis_transform = transforms.Compose([
+    transforms.Resize(32),
+    transforms.CenterCrop(32)
+])
 test_transform = transforms.Compose([
     transforms.Resize(32),
     transforms.CenterCrop(32),
@@ -25,14 +29,27 @@ cnn.load_state_dict(torch.load('checkpoints/bam-train-gtsrb-test-64_resnet18_1_1
 cnn = cnn.cuda()
 cnn.eval()
 
-image_ = Image.open('datasets/BAM_data/images/03053.jpg')
-for i in tqdm(range(1000)):
-    image = test_transform(image_)
-    test_loader = data.DataLoader(dataset=[image],
-                              batch_size=1)
+#image_ = Image.open('datasets/BAM_data/images/03053.jpg')
+cap = cv2.VideoCapture(0)
+while(True):
+    ret, frame = cap.read()
+
+    frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    frame = Image.fromarray(frame)
+
+    # Display the frame
+    vis = np.array(vis_transform(frame))
+    vis = cv2.cvtColor(vis,cv2.COLOR_RGB2BGR)
+    cv2.imshow('frame', vis)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    # Do inference
+    frame = test_transform(frame)
+    test_loader = data.DataLoader(dataset=[frame], batch_size=1)
     for image in test_loader:
         image = Variable(image, volatile=True).cuda()
         pred = cnn(image)
         pred = nn.functional.softmax(pred,dim=1)
         conscore = pred.data[0,1].item()
-#        print(conscore)
+        print(conscore)
--- codes/final_clean/readme.md
+++ codes/final_clean/readme.md
@@ -1,8 +1,5 @@
-# ONLY ADD FINAL (READABLE) CODE THAT WORKS
-1. Download the original pytorch-cutout from https://github.com/uoguelph-mlrg/Cutout
-2. Replace train.py with the new one
-3. Add gtsrb_dataloader.py
-4. Add a new forder named 'data', then put into our datset
-5. Add a new forder named 'logs'
-6. Add a new forder named 'checkpoints'
-7. Add the 'imblean' folder from https://github.com/scikit-learn-contrib/imbalanced-learn
+# Instructions
+
+1. Put BAM dataset images in `datasets/images` folder
+2. Run `train.py`
+3. Run `demo.py`
